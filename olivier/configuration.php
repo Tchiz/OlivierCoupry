db1289de9dea3cf5a502083e4b8e7a2b0d158dfa
cra <cra@voltalis.com>
2013-03-19 16:01:48 +0100

    delete source code about Olivier Coupry's first version web site

--- olivier/configuration.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("DOMAIN", "mysql51-30.perso");
-	define("USER", "oliviercxbddoc");
-	define("MDP", "olivier15");
-	define("BDD", "oliviercxbddoc");
-?>
--- olivier/verifminis.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include("config.php");
-	$serie = $_POST['s'];
-	$temp = requete("SELECT count(*) as counter FROM photo WHERE id_serie_photo=(SELECT id_serie FROM serie WHERE libelle_serie='".$serie."')");
-	while($data = mysql_fetch_array($temp))
-	{
-		echo $data['counter'];
-	}
-?>
